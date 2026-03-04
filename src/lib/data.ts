75dbc1971dc74f15105b538ca001ef3abe68e00d
kfischi <kfir.biz@gmail.com>
2026-03-04 23:31:37 +0200

    Create data.ts

---
+++ src/lib/data.ts
@@ -0,0 +1,17 @@
+export const properties = {
+  caesarea: {
+    name: "אחוזת הקיסר",
+    themeColor: "#D4AF37", // זהב
+    bgImage: "https://images.unsplash.com/photo-1600607687940-477a63bd394c",
+    description: "חוויה קיסרית בין עמודי אבן לים הפתוח.",
+    affiliateLink: "https://zimer360.com/caesarea"
+  },
+  forest: {
+    name: "בקתות היער",
+    themeColor: "#2D4B2D", // ירוק יער
+    bgImage: "https://images.unsplash.com/photo-1449156001437-3a1661acda22",
+    description: "שלווה אורגנית בלב החורש הצפוני.",
+    affiliateLink: "https://zimer360.com/forest"
+  }
+  // ... וכן הלאה לשאר ה-4
+};
