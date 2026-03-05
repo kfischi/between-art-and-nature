c2fbd23e083ae5e020e3296fe1f7871f50854e89
kfischi <kfir.biz@gmail.com>
2026-03-05 11:13:28 +0200

    Update locations.ts

--- src/lib/locations.ts
+++ src/lib/locations.ts
@@ -5,39 +5,25 @@ export const locations = {
     name: "אחוזה בגליל",
     description: "חוויית אירוח סינמטית המשלבת אדריכלות אבן מקומית עם עיצוב מודרני מינימליסטי, אל מול נופי הגליל העוצמתיים.",
     image: "https://res.cloudinary.com/decirk3zb/image/upload/v1772401384/1_h9zhkx.webp",
-    imageFeatured: "https://res.cloudinary.com/decirk3zb/image/upload/v1772401403/10_wkxsda.webp",
+    imageFeatured: "https://res.cloudinary.com/decirk3zb/image/upload/v1772401401/9_mwbfrq.webp",
     gallery: [
-      "https://res.cloudinary.com/decirk3zb/image/upload/v1772401384/1_h9zhkx.webp",
-      "https://res.cloudinary.com/decirk3zb/image/upload/v1772401385/2_wm9hcm.webp",
       "https://res.cloudinary.com/decirk3zb/image/upload/v1772401386/3_tgup62.webp",
       "https://res.cloudinary.com/decirk3zb/image/upload/v1772401389/4_lx73rn.webp",
-      "https://res.cloudinary.com/decirk3zb/image/upload/v1772401391/5_oc93v5.webp",
-      "https://res.cloudinary.com/decirk3zb/image/upload/v1772401392/6_bvifvx.webp",
-      "https://res.cloudinary.com/decirk3zb/image/upload/v1772401396/7_uv1fqk.webp",
-      "https://res.cloudinary.com/decirk3zb/image/upload/v1772401399/8_muhjiq.webp",
-      "https://res.cloudinary.com/decirk3zb/image/upload/v1772401401/9_mwbfrq.webp",
-      "https://res.cloudinary.com/decirk3zb/image/upload/v1772401403/10_wkxsda.webp"
+      "https://res.cloudinary.com/decirk3zb/image/upload/v1772401399/8_muhjiq.webp"
     ],
-    seoKeywords: ["אחוזה בגליל", "אירוח יוקרה", "גליל עליון", "אדריכלות"]
+    seoKeywords: ["אחוזה בגליל", "אירוח יוקרה", "גליל עליון"]
   },
   'pekiin': {
     name: "פקיעין",
     description: "בין סמטאות עתיקות לטבע פראי, מתחם האירוח בפקיעין מציע חיבור עמוק לשורשים ולאומנות מקומית.",
     image: "https://res.cloudinary.com/decirk3zb/image/upload/v1772466314/2_g8wy1q.jpg",
-    imageFeatured: "https://res.cloudinary.com/decirk3zb/image/upload/v1772466328/11_xsavtz.jpg",
+    imageFeatured: "https://res.cloudinary.com/decirk3zb/image/upload/v1772466322/7_a09vce.jpg",
     gallery: [
-      "https://res.cloudinary.com/decirk3zb/image/upload/v1772466314/2_g8wy1q.jpg",
-      "https://res.cloudinary.com/decirk3zb/image/upload/v1772466316/3_bzl0rc.jpg",
-      "https://res.cloudinary.com/decirk3zb/image/upload/v1772466316/4_htedhv.jpg",
-      "https://res.cloudinary.com/decirk3zb/image/upload/v1772466318/5_u599ne.jpg",
       "https://res.cloudinary.com/decirk3zb/image/upload/v1772466320/6_gsbvpo.jpg",
-      "https://res.cloudinary.com/decirk3zb/image/upload/v1772466322/7_a09vce.jpg",
-      "https://res.cloudinary.com/decirk3zb/image/upload/v1772466323/8_vyhyhm.jpg",
       "https://res.cloudinary.com/decirk3zb/image/upload/v1772466325/9_bqxpwo.jpg",
-      "https://res.cloudinary.com/decirk3zb/image/upload/v1772466327/10_fvnm0t.jpg",
-      "https://res.cloudinary.com/decirk3zb/image/upload/v1772466328/11_xsavtz.jpg"
+      "https://res.cloudinary.com/decirk3zb/image/upload/v1772466316/4_htedhv.jpg"
     ],
-    seoKeywords: ["פקיעין", "אירוח אותנטי", "סוויטות יוקרה", "אומנות"]
+    seoKeywords: ["פקיעין", "אירוח אותנטי", "סוויטות יוקרה"]
   },
   'zuriel': {
     name: "אחוזת צוריאל",
@@ -45,8 +31,6 @@ export const locations = {
     image: "https://res.cloudinary.com/decirk3zb/image/upload/v1771705570/WhatsApp_Image_2026-02-19_at_22.03.38_bbqqag.jpg",
     imageFeatured: "https://res.cloudinary.com/decirk3zb/image/upload/v1771705569/WhatsApp_Image_2026-02-19_at_22.03.38_6_uhgvgs.jpg",
     gallery: [
-      "https://res.cloudinary.com/decirk3zb/image/upload/v1771705570/WhatsApp_Image_2026-02-19_at_22.03.38_bbqqag.jpg",
-      "https://res.cloudinary.com/decirk3zb/image/upload/v1771705569/WhatsApp_Image_2026-02-19_at_22.03.38_6_uhgvgs.jpg",
       "https://res.cloudinary.com/decirk3zb/image/upload/v1771705557/WhatsApp_Image_2026-02-19_at_22.03.37_4_wavzxr.jpg",
       "https://res.cloudinary.com/decirk3zb/image/upload/v1771705555/WhatsApp_Image_2026-02-19_at_22.03.36_6_onigmj.jpg",
       "https://res.cloudinary.com/decirk3zb/image/upload/v1771705556/WhatsApp_Image_2026-02-19_at_22.03.37_2_p2a2vl.jpg",
@@ -58,11 +42,30 @@ export const locations = {
       "https://res.cloudinary.com/decirk3zb/image/upload/v1771707164/1_q49spa.jpg",
       "https://res.cloudinary.com/decirk3zb/image/upload/v1771707165/3_fvqxuv.jpg"
     ],
-    seoKeywords: ["אחוזת צוריאל", "וילות יוקרה", "גליל מערבי", "אירוח מעוצב"]
+    seoKeywords: ["אחוזת צוריאל", "וילות יוקרה", "גליל מערבי"]
+  },
+  'clementine': {
+    name: "אחוזת קלמנטינה",
+    description: "אלגנטיות כפרית בלב המושבה. אחוזת קלמנטינה מציעה חוויה אינטימית ועוטפת, עם תשומת לב לפרטים הקטנים ביותר.",
+    image: "https://res.cloudinary.com/decirk3zb/image/upload/v1772466328/11_xsavtz.jpg",
+    imageFeatured: "https://res.cloudinary.com/decirk3zb/image/upload/v1772466327/10_fvnm0t.jpg",
+    gallery: [
+      "https://res.cloudinary.com/decirk3zb/image/upload/v1772466328/11_xsavtz.jpg",
+      "https://res.cloudinary.com/decirk3zb/image/upload/v1772466327/10_fvnm0t.jpg",
+      "https://res.cloudinary.com/decirk3zb/image/upload/v1772466323/8_vyhyhm.jpg",
+      "https://res.cloudinary.com/decirk3zb/image/upload/v1772466316/3_bzl0rc.jpg",
+      "https://res.cloudinary.com/decirk3zb/image/upload/v1772466318/5_u599ne.jpg",
+      "https://res.cloudinary.com/decirk3zb/image/upload/v1772401403/10_wkxsda.webp",
+      "https://res.cloudinary.com/decirk3zb/image/upload/v1772401385/2_wm9hcm.webp",
+      "https://res.cloudinary.com/decirk3zb/image/upload/v1772401384/1_h9zhkx.webp",
+      "https://res.cloudinary.com/decirk3zb/image/upload/v1772401392/6_bvifvx.webp",
+      "https://res.cloudinary.com/decirk3zb/image/upload/v1772401396/7_uv1fqk.webp",
+      "https://res.cloudinary.com/decirk3zb/image/upload/v1772401391/5_oc93v5.webp"
+    ],
+    seoKeywords: ["אחוזת קלמנטינה", "חופשה רומנטית", "אירוח יוקרה"]
   }
 };
 
-// פונקציית העזר לשליפת מידע - קריטית למניעת שגיאות Build
 export function getLocation(slug: string) {
   const location = locations[slug as keyof typeof locations];
   if (!location) return null;
